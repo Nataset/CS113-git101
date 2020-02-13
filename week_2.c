8b2336ce141bfb81aa43cdd4b466f40ba97f5689
TroNine <nataset.t@ku.th>
2020-02-13 23:48:20 +0700

    input, atoi?, while_loop, for_loop

---
+++ week_2.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+ char name[20];
+int main()
+{
+    // ------- input --------//
+    // printf ( "Enter name :" );
+    // gets ( name );
+    // fgets ( name, 20 stdin );
+    char number[5];
+    // fgets( number, 5, stdin );
+    // int i = atoi( number );
+    // printf( "number = %s, i = %d\n", number, i );
+    // char a = getchar();
+    // getchar();
+    // char b = getchar();
+    // array จะมี \0 ตามหลังเสมอเวลาใช้ fgets ( -, x, - ) ตำแหน่ง x ดูให้ดี
+    
+    //------- ato? -------//
+    // มาจาก array to int,float อะไรก็ว่าไปอยู่ใน stdlib.h
+    // long 1 = atoi(number);
+    // double d = atof(number);
+
+    // puts( number );
+    // putchar( 100 ); putchar( 'a' );
+    // int found = 0;
+    // float isExit = 0.0;
+    // char isOnline = '\0';
+
+    //-------ตัวอย่าง---------//
+    // fgets( number, 5, stdin );
+    // int i = atoi( number );
+    // if ( i >= 0 && i <=10 ){
+    //     printf( "if = %d", i >= 0 && i <= 10 );
+    // }else if ( i <= 50 || !found ){
+    //     printf( "else if" );
+    // }else {
+    //     printf( "else" );
+    // }
+
+    fgets( number, 5, stdin );
+    int i = atoi( number );
+    // int count = 0;
+    // while ( count < i ){
+    //     printf( "%d\n", count );
+    //     count++;
+    // }
+    // for ( int j = 0; j <i; j++ ){
+    //     printf( "%d\n", j );
+    // }
+    // while (1);{
+    //     infinite loop
+    // }
+}
