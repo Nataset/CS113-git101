4ef7fb7abad1344eebc0df10df19a10a419df348
TroNine <nataset.t@ku.th>
2020-02-22 11:12:12 +0700

    C str,ch library

---
+++ week_10_2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main () {
+    //-------------character Library---------
+    //*******ctpe.h**********
+    // isdigit('char') return 0,1 ตัวเลข
+    // isalpha('char') return 0,1 ตัวอักศร
+    // isalnum('char') return 0,1 ตัวอักศรและตัวเลข
+    // isxdigit('char') return 0,1 ตัวเลขฐาน 16
+    // islower('char') return 0,1 
+    // isupper('char') return 0,1
+    // tolower('char') return char
+    // toupper('char') return char
+
+    // ตัวอักขระพิเศษ 
+    
+}
